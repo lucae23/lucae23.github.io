42acababd61586022a53624066e4a85580cc8ea1
lucae23 <162288707+lucae23@users.noreply.github.com>
2025-04-17 11:59:17 +1000

    add pause

--- week7/videoscript.js
+++ week7/videoscript.js
@@ -16,3 +16,21 @@ pauseButton.addEventListener("click", pauseVideo);
 function pauseVideo() {
   myVideo.pause();
 }
+
+const playPauseButton = document.querySelector("#play-pause-btn");
+console.log(playPauseButton);
+
+const playPauseImg = document.querySelector("#play-pause-img");
+console.log(playPauseImg);
+// Event listener to toggle between playing and pausing video on clicking the button
+playPauseButton.addEventListener("click", toggleVideo);
+
+function toggleVideo() {
+  if (myVideo.paused || myVideo.ended) {
+    myVideo.play();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/no-audio--v1.png";
+  } else {
+    myVideo.pause();
+    playPauseImg.src = "https://img.icons8.com/ios-glyphs/30/play--v1.png";
+  }
+}
